100935c4ea40d2b3b2601683d7e8230b666ec6a2
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-11-10 04:09:55 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,7 +19,7 @@ def bonus
       ]
    }
   }
-
+end
  
 #   epic_tragedy = [
 #   :montague => {
@@ -46,4 +46,4 @@ def bonus
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  epic_tragedy[]
+  #epic_tragedy[]
